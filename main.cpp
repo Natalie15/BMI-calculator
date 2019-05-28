0b4330ac91d91959e9348ad2d687585a0297128a
Natalie Weishuhn <n.weishuhn1@gmail.com>
2019-05-28 16:58:45 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  
+  double height, weight, bmi;
+  
+  // Ask user for their height
+  
+  std::cout << "Type in your height (in): ";
+  std::cin >> height;
+  
+  // Now ask the user for their weight and calculate BMI
+  std::cout << "Type in your weight (lbs): ";
+  std::cin >> weight;
+  
+  bmi = weight/(height*height);
+  std::cout << "Your BMI is " << bmi << "\n";
+
+  return 0;
+
+}
